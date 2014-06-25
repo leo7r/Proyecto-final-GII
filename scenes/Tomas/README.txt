fa972c90e7709512c0d261e565b324bc7c01563c
TroiKM <karentroiano2009@gmail.com>
2014-06-24 23:14:23 -0430

    Tomas

---
+++ scenes/Tomas/README.txt
@@ -0,0 +1,6 @@
+tomaPista01 : Partida con la bandera de la meta.
+tomaPista02 : Tunel de lego, varios carros. Caida. 
+tomaPista03 : Levantamiento.
+FALTA PARTICULAS -> tomaPista04 : Choque cubo carro.
+FALTA ILUMINACION (Esperando por Roa) -> tomaPista05 : Agarrando monedas.Peces saltando. Shell
+FALTA -> tomaBalaMov01 : Bala moviendo en la pista.
--- scenes/Tomas/renderBala01.bat
+++ scenes/Tomas/renderBala01.bat
@@ -1,1 +1,11 @@
-"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera1 -s 0 -e 86 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaBala01.mb"
+"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera1 -s 1 -e 75 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaMovBala01.mb"
+
+"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera1 -s 1 -e 42 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista03.mb"
+
+"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera1 -s 50 -e 150 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista01.mb"
+
+"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera1 -s 1 -e 100 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista02.mb"
+
+# "C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera1 -s 1 -e 50 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista04.mb"
+
+"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 1280 -y 720 -cam camera2 -s 1 -e 150 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista05.mb"
