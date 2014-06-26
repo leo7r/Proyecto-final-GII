0445e350bb86f0ebe01bc5c81e0795617005aba4
TroiKM <karentroiano2009@gmail.com>
2014-06-26 06:47:29 -0430

    Más tomas

---
+++ scenes/Tomas/renderParaKaren.bat
@@ -0,0 +1,1 @@
+"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 960 -y 540 -cam camera1 -s 1 -e 24 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaMoneda01.mb"
--- scenes/Tomas/renderParaLeonardo.bat
+++ scenes/Tomas/renderParaLeonardo.bat
@@ -1,3 +1,1 @@
-"C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 960 -y 540 -cam camera1 -s 1 -e 100 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista02.mb"
-
 "C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 8 -x 960 -y 540 -cam camera2 -s 22 -e 150 -proj "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII" "C:/Users/Karen Troiano/Documents/GitHub/Proyecto-final-GII/scenes/Tomas/tomaPista05.mb"
